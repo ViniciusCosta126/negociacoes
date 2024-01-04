25ed84766267ef3790f68da33a961bfd57f0237c
Vinicius Costa <54358510+ViniciusCosta126@users.noreply.github.com>
2024-01-04 12:23:47 -0300

    Simplificando ainda mais o codigo das classes

--- app/app.ts
+++ app/app.ts
@@ -1,10 +1,9 @@
-import { NegociacaoController } from './controllers/NegociacaoController.js'
+import { NegociacaoController } from "./controllers/NegociacaoController.js";
 
-const controller = new NegociacaoController()
+const controller = new NegociacaoController();
 
-
-const form = document.querySelector(".form")
-form.addEventListener('submit', (event) => {
-    event.preventDefault()
-    controller.adiciona()
-})
+const form = document.querySelector(".form");
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  controller.adiciona();
+});
--- dist/js/controllers/NegociacaoController.js
+++ dist/js/controllers/NegociacaoController.js
@@ -3,9 +3,9 @@ import { Negociacoes } from "../models/negociacoes.js";
 export class NegociacaoController {
     constructor() {
         this.negociacoes = new Negociacoes();
-        this.inputData = document.querySelector('#data');
-        this.inputQuantidade = document.querySelector('#quantidade');
-        this.inputValor = document.querySelector('#valor');
+        this.inputData = document.querySelector("#data");
+        this.inputQuantidade = document.querySelector("#quantidade");
+        this.inputValor = document.querySelector("#valor");
     }
     adiciona() {
         const negociacao = this.criaNegociacao();
@@ -15,15 +15,15 @@ export class NegociacaoController {
     }
     criaNegociacao() {
         const exp = /-/g;
-        const date = new Date(this.inputData.value.replace(exp, ','));
+        const date = new Date(this.inputData.value.replace(exp, ","));
         const quantidade = parseInt(this.inputQuantidade.value);
         const valor = parseFloat(this.inputValor.value);
         return new Negociacao(date, quantidade, valor);
     }
     limparFormulario() {
-        this.inputData.value = '';
-        this.inputQuantidade.value = '';
-        this.inputValor.value = '';
+        this.inputData.value = "";
+        this.inputQuantidade.value = "";
+        this.inputValor.value = "";
         this.inputData.focus();
     }
 }
