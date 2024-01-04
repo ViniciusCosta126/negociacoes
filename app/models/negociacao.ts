e5c2f6d1ec4c65aded827d85e43fb2f5353825bf
Vinicius Costa <54358510+ViniciusCosta126@users.noreply.github.com>
2024-01-04 12:30:54 -0300

    Protegendo propriedaes privadas'

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,9 +1,14 @@
 export class Negociacao {
   constructor(
-    public readonly data: Date,
+    public readonly _data: Date,
     public readonly quantidade: number,
     public readonly valor: number
   ) {}
+
+  get data(): Date {
+    const dataNova = new Date(this._data.getTime());
+    return dataNova;
+  }
 
   get volume(): number {
     return this.quantidade * this.valor;
--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -1,8 +1,12 @@
 export class Negociacao {
-    constructor(data, quantidade, valor) {
-        this.data = data;
+    constructor(_data, quantidade, valor) {
+        this._data = _data;
         this.quantidade = quantidade;
         this.valor = valor;
+    }
+    get data() {
+        const dataNova = new Date(this._data.getTime());
+        return dataNova;
     }
     get volume() {
         return this.quantidade * this.valor;
